ee60e2759406871de88c8ca15048cd70aaf2964e
Al Zee <z@alz.ee>
2022-01-21 00:07:18 +0800

    ionic cordova resources android --force

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'app.syzsss.cn',
-  appName: 'zsapp',
+  appId: 'app.syzszz.cn',
+  appName: '十堰再生资源',
   webDir: 'www',
   bundledWebRuntime: false
 };
