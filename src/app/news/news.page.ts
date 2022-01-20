17f7e1837e8ee93317e3881eeb3c07bdff7c207e
Al Zee <z@alz.ee>
2022-01-21 02:06:24 +0800

    nth

--- src/app/news/news.page.ts
+++ src/app/news/news.page.ts
@@ -7,15 +7,115 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewsPage implements OnInit {
   seg = 1;
+  nodes: any;
+  nodes1: any;
+  nodes2: any;
+  nodes3: any;
 
   constructor() { }
 
   ngOnInit() {
+    this.nodes1 = [
+      {
+        title: 'daf adsf ',
+        author: 'adsfm',
+        date: '123 123'
+      },
+      {
+        title: 'daf adsf ',
+        author: 'adsfm',
+        date: '123 123'
+      },
+      {
+        title: 'daf adsf ',
+        author: 'adsfm',
+        date: '123 123'
+      },
+      {
+        title: 'daf adsf ',
+        author: 'adsfm',
+        date: '123 123'
+      },
+      {
+        title: 'daf adsf ',
+        author: 'adsfm',
+        date: '123 123'
+      },
+      {
+        title: 'daf adsf ',
+        author: 'adsfm',
+        date: '123 123'
+      },
+      {
+        title: 'daf adsf ',
+        author: 'adsfm',
+        date: '123 123'
+      },
+    ];
+    this.nodes2 = [
+      {
+        title: 'daf adsf ',
+        author: 'adsfm',
+        date: '123 123'
+      },
+      {
+        title: 'daf adsf ',
+        author: 'adsfm',
+        date: '123 123'
+      },
+      {
+        title: 'daf adsf ',
+        author: 'adsfm',
+        date: '123 123'
+      },
+      {
+        title: 'daf adsf ',
+        author: 'adsfm',
+        date: '123 123'
+      },
+      {
+        title: 'daf adsf ',
+        author: 'adsfm',
+        date: '123 123'
+      },
+    ];
+    this.nodes3 = [
+      {
+        title: 'daf adsf ',
+        author: 'adsfm',
+        date: '123 123'
+      },
+      {
+        title: 'daf adsf ',
+        author: 'adsfm',
+        date: '123 123'
+      },
+      {
+        title: 'daf adsf ',
+        author: 'adsfm',
+        date: '123 123'
+      },
+    ];
+
+    this.nodes = this.nodes1;
   }
 
   segmentChanged(e){
     this.seg = +e.detail.value;
     console.log(this.seg);
+    switch (this.seg) {
+      case 1:
+        this.nodes = this.nodes1;
+        break;
+      case 2:
+        this.nodes = this.nodes2;
+        break;
+      case 3:
+        this.nodes = this.nodes3;
+        break;
+    }
   }
 
+  loadData(e){
+  }
 }
