82ed2aadf76ec4e73575470c3591fee0935a2803
Al Zee <z@alz.ee>
2022-01-20 21:09:24 +0800

    nth

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'app.syzsss.cn',
+  appName: 'zsapp',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
