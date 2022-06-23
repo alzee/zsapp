133c957607f7b533f136deaeee20b85595f20759
Al Zee <z@alz.ee>
2022-06-23 15:23:53 +0800

    m.syzszz.cn

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,7 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-const url = 'https://zmall.itove.com';
+const url = 'https://m.syzszz.cn';
 export const environment = {
   production: false,
   smsPass: 'test',
